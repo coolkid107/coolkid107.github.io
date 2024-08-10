3a92873defccf866de5b60d233b982323e5558fe
coolkid107 <localuniverse@icloud.com>
2024-08-09 19:47:23 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// log message to the console //
+
+let copyright = 'Copyright 2024 Astryllly'
+let ASCII = `
+
+     _____            __                 .__  .__         
+    /  _  \   _______/  |________ ___.__.|  | |  | ___.__.
+   /  /_\  \ /  ___/\   __\_  __ <   |  ||  | |  |<   |  |
+  /    |    \\___ \  |  |  |  | \/\___  ||  |_|  |_\___  |
+  \____|__  /____  > |__|  |__|   / ____||____/____/ ____|
+          \/     \/               \/               \/     
+
+  `
+
+let version_info = 'Version: 1.0.0'
+let version_date = 'Version Date: 2024'
+//actually log the message to the console//
+
+console.log(version_info)
+console.log(copyright);
+console.log(ASCII);
+console.log(version_date);
+
+//-----------------------------------//
+
+
+
